d9bbc950258593417c9a00a7942ea842ada3d72d
Steven Gregory <sgregory@gmail.com>
2019-04-11 13:54:53 -0500

    Expose target language arg

--- src/translate.js
+++ src/translate.js
@@ -4,21 +4,21 @@ import LanguageTranslatorV3 from 'watson-developer-cloud/language-translator/v3'
 
 dotenv.config();
 
-function params(text = 'hola', source = 'en', target = 'es') {
+function params(text = 'hola', target = 'es', source = 'en') {
   return {
     text,
-    source,
-    target
+    target,
+    source
   };
 }
 
-async function doTranslation(text) {
+async function doTranslation(text, target) {
   const languageTranslator = new LanguageTranslatorV3({
     username: process.env.LANGUAGE_TRANSLATOR_USERNAME,
     password: process.env.LANGUAGE_TRANSLATOR_PASSWORD,
     version: process.env.LANGUAGE_TRANSLATOR_VERSION
   });
-  const body = await languageTranslator.translate(params(text));
+  const body = await languageTranslator.translate(params(text, target));
   console.log(chalk.cyan(body.translations[0].translation));
 }
 
