c38eb17c2e713764fe6d6864670824934cfe5666
Steven Gregory <sgregory@gmail.com>
2019-04-10 22:38:11 -0500

    Add new translate file

---
+++ translate.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import LanguageTranslatorV3 from 'watson-developer-cloud/language-translator/v3';
+
+dotenv.config();
+
+function params(text = 'hola', source = 'en', target = 'es') {
+  return {
+    text,
+    source,
+    target
+  };
+}
+
+async function doTranslation(text) {
+  const languageTranslator = new LanguageTranslatorV3({
+    username: process.env.LANGUAGE_TRANSLATOR_USERNAME,
+    password: process.env.LANGUAGE_TRANSLATOR_PASSWORD,
+    version: '2019-01-10'
+  });
+  const body = await languageTranslator.translate(params(text));
+  console.log(body.translations[0].translation);
+}
+
+export default doTranslation;
