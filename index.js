ff68a4ab5a2d06a5d8761d8709192288493712d3
Steven Gregory <sgregory@gmail.com>
2019-04-10 22:37:39 -0500

    Move translation logic to new file

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 import prompt from 'prompt';
-import dotenv from 'dotenv';
-import LanguageTranslatorV3 from 'watson-developer-cloud/language-translator/v3';
+import doTranslation from './translate';
 
-dotenv.config();
-
-const languageTranslator = new LanguageTranslatorV3({
-  username: process.env.LANGUAGE_TRANSLATOR_USERNAME,
-  password: process.env.LANGUAGE_TRANSLATOR_PASSWORD,
-  version: '2019-01-10'
-});
-
-function params(text = 'hola', source = 'en', target = 'es') {
-  return {
-    text,
-    source,
-    target
-  };
-}
-
-async function callTranslate(text) {
-  const body = await languageTranslator.translate(params(text));
-  console.log(body.translations[0].translation);
-}
-
-async function init() {
+function init() {
   prompt.start();
-  await prompt.get(['msg'], function(err, result) {
-    callTranslate(result.msg);
+  prompt.get(['msg'], function(err, result) {
+    doTranslation(result.msg);
   });
 }
 
