7d4bbbdec2e7b4e3d884a6cd917a1106ed773c51
Steven Gregory <sgregory@gmail.com>
2019-04-23 18:00:56 -0500

    Moved service data to fn & refactoring

--- src/translate.js
+++ src/translate.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import LanguageTranslatorV3 from 'watson-developer-cloud/language-translator/v3';
 import config from './config.js';
 
-function params(text) {
+function getMessageData(text) {
   return {
     text,
     source: config.source,
@@ -10,13 +10,17 @@ function params(text) {
   };
 }
 
-async function doTranslation(text) {
-  const languageTranslator = new LanguageTranslatorV3({
+function getServiceData() {
+  return {
     iam_apikey: config.apiKey,
     url: config.url,
     version: config.version
-  });
-  const body = await languageTranslator.translate(params(text));
+  };
+}
+
+async function doTranslation(text) {
+  const languageTranslator = new LanguageTranslatorV3(getServiceData());
+  const body = await languageTranslator.translate(getMessageData(text));
   console.log(chalk.cyan(body.translations[0].translation));
 }
 
